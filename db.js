9809069dba7451b21f1bef2f513f4266345795f4
Irvin <reimer1693@gmail.com>
2022-10-18 16:03:44 -0500

    variable port añadida

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2/promise"
 
 export const pool =  createPool({
     host:'radionortechico.com',
-    port:3306,
+    port:`0.0.0.0:$PORT`|| 3306,
     user:'wlqgsapm_reimer',
     password:'kovenant16',
     database:'wlqgsapm_prueba'
