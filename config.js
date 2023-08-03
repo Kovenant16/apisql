4d6138bf0bcbbbfdfb2762c964ac72795c53ba4f
Irvin <reimer1693@gmail.com>
2023-08-02 23:38:54 -0500

    commit 02 de agosto

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 4000;
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '949972514'
-export const DB_NAME = process.env.DB_NAME || 'tasksd'
-export const DB_PORT = process.env.DB_PORT || 3306
+export const PORT = 4000;
+export const DB_HOST = "radionortechico.com"
+export const DB_USER = "wlqgsapm_reimer"
+export const DB_PASSWORD = 'aponte949972514'
+export const DB_NAME = 'wlqgsapm_waras'
+export const DB_PORT = 3306
